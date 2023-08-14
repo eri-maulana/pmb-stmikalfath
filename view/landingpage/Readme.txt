d2a4eecde0cc90f9c2fe5d02cf5d6135f16aef2e
unknown <malanabouble11@gmail.com>
2023-08-14 07:24:32 +0700

    Upload Pertama Kali

---
+++ view/landingpage/Readme.txt
@@ -0,0 +1,12 @@
+Thanks for downloading this template!
+
+Template Name: Moderna
+Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
+
+
+This is Modify By
+Eri Maulana and Putri Narila from STMIK AL FATH SUKABUMI ^_^
+PLEASE ... DO NOT REMOVE THIS FILE !! 
+you can download this project on repository
---
+++ view/template/footer.php
@@ -0,0 +1,27 @@
+          </div>
+          <footer class="main-footer">
+             <div class="row">
+                <div class="col-4">
+                   Copyright &copy; 2018 <div class="bullet"></div> <br> Design By <a href="https://nauval.in/">Muhamad
+                      Nauval
+                      Azhar</a>
+                </div>
+                <div class="col-4">
+                   Modify by <br>
+                   <a href="https://www.instagram.com/erimaulana.69/" class="text-dark">Eri Maulana</a>
+                   &
+                   <a href="https://www.instagram.com/narila_putnar02/" class="text-dark">Putri Narila</a>
+                </div>
+                <div class="col-4">
+                   Developer By <br> <a href="//linkedin.com/in/bejosuseno" target="blank">Code Peka</a>
+                   <!-- Create By <a href="#">Code Peka</a> -->
+                </div>
+             </div>
+
+          </footer>
+          </div>
+          </div>
+
+          </body>
+
+          </html>
